c971eaf38610cb6cbeca0d1835f5d6cfcb454927
Mehrubon <nabievm811@gmail.com>
2025-09-11 12:25:17 +0300

    Update merge-two-sorted-lists.py

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -5,25 +5,21 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        # Собираем значения из первого списка
         ls1 = []
         current1 = list1
         while current1:
             ls1.append(current1.val)
             current1 = current1.next
         
-        # Собираем значения из второго списка
         ls2 = []
         current2 = list2
         while current2:
             ls2.append(current2.val)
             current2 = current2.next
         
-        # Объединяем и сортируем
         ls = ls1 + ls2
         ls = sorted(ls)
         
-        # Создаем новый связный список
         dummy = ListNode()
         current = dummy
         for val in ls:
