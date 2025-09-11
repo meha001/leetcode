44c0d7ac1e1f1b75e13bc8f4cf0bd749ff50075e
Mehrubon <nabievm811@gmail.com>
2025-09-11 12:24:51 +0300

    Update merge-two-sorted-lists.py

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -1,8 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         """
@@ -10,8 +5,29 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        list3 = list1 + list2
-        return sorted(list3)
-
-t = Solution()
-print(t.mergeTwoLists([1, 2, 3], [1, 2, 3]))
+        # Собираем значения из первого списка
+        ls1 = []
+        current1 = list1
+        while current1:
+            ls1.append(current1.val)
+            current1 = current1.next
+        
+        # Собираем значения из второго списка
+        ls2 = []
+        current2 = list2
+        while current2:
+            ls2.append(current2.val)
+            current2 = current2.next
+        
+        # Объединяем и сортируем
+        ls = ls1 + ls2
+        ls = sorted(ls)
+        
+        # Создаем новый связный список
+        dummy = ListNode()
+        current = dummy
+        for val in ls:
+            current.next = ListNode(val)
+            current = current.next
+        
+        return dummy.next
