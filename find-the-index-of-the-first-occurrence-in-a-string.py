542c97616f7e884834e4b246ceafcf0ca61069f4
Mehrubon <nabievm811@gmail.com>
2025-09-11 15:09:19 +0300

    .

---
+++ find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.index(needle) if needle in haystack else -1
+    
+    
+    
+    
+    
+    
+print(Solution().strStr(input(), input()))
---
+++ sqrtx.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:return 0
+        i = 2
+        while i ** 2 <= x:
+            i += 1
+        return i - 1
+    
+
+print(Solution().mySqrt(100))
