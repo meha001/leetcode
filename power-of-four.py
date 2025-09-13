0453ac3e05c16aad84a04346bbed1159c4f4fca3
Mehrubon <nabievm811@gmail.com>
2025-09-13 14:38:41 +0300

    Create power-of-four.py

---
+++ power-of-four.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return bin(n)[2:].count('1') == 1 and bin(n)[2:].count("0") % 2 == 0 and n >= 0
+    
+print(Solution().isPowerOfFour(int(input())))
