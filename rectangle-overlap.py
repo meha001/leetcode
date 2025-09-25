84d569a1af2aa42609dde24430751a887fa52d53
Mehrubon <nabievm811@gmail.com>
2025-09-25 15:46:40 +0300

    Update rectangle-overlap.py

--- rectangle-overlap.py
+++ rectangle-overlap.py
@@ -3,4 +3,7 @@ __author__ = 'meha001'
 
 class Solution:
     def isRectangleOverlap(self, rec1: list[int], rec2: list[int]) -> bool:
-        fo
+        return (rec1[0] < rec2[2] and  
+                rec2[0] < rec1[2] and  
+                rec1[1] < rec2[3] and  
+                rec2[1] < rec1[3])     
