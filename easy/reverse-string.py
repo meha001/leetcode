93ef0ae35fe361db9768818608ba7f7f45b7769b
Mehrubon <nabievm811@gmail.com>
2025-09-28 13:05:17 +0300

    1

---
+++ easy/reverse-string.py
@@ -0,0 +1,10 @@
+__author__ = 'meha001'
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
+        
+            
+
