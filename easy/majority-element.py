a8e776dc0a07ab338e29ff2416085021a3ada0f9
meha001 <123616994+meha001@users.noreply.github.com>
2025-10-04 20:53:07 +0300

    1

--- easy/majority-element.py
+++ easy/majority-element.py
@@ -2,5 +2,14 @@ __author__ = 'meha001'
 
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        nums = sorted(nums)
-        
+        t = dict()
+        n = len(nums)
+        for i in range(n):
+            if f"{nums[i]}" in t:
+                t[f"{nums[i]}"] = t[f"{nums[i]}"] + 1
+            else:
+                t[f"{nums[i]}"] = 1
+            if n // 2 < t[f"{nums[i]}"]:
+                return  nums[i]
+            
+print(Solution().majorityElement([3, 2, 3]))    
---
+++ easy/pascals-triangle.py
@@ -0,0 +1,19 @@
+__author__ = 'meha001'
+
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        t = [[1], [1, 1]]
+        if numRows == 1:return [t[0]]
+        elif numRows == 2: return t
+        else:
+            while len(t) != numRows:
+                tlis = [1]
+                for i in range(len(t[-1])-1):
+                    tlis.append(t[-1][i] + t[-1][i+1])
+                tlis.append(1)
+                t.append(tlis)
+            return t
+
+print(Solution().generate(5))
+
+# __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
